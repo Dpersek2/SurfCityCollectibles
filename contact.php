3345bbe803f8bfe3c2eb85c44342a21a5d641a9a
Dpersek2 <130072731+Dpersek2@users.noreply.github.com>
2025-04-26 10:35:24 -0400

    Add files via upload

--- contact.php
+++ contact.php
@@ -26,13 +26,8 @@
 
 </main>
 
-<footer class="bg-[#247A73] text-white text-center p-4 mt-16">
-  &copy; 2025 SurfCity Collectibles. All rights reserved.
-  <br>
-  <a href="index.php" class="underline hover:text-gray-200">Home</a> |
-  <a href="shop.php" class="underline hover:text-gray-200">Shop</a> |
-  <a href="cart.php" class="underline hover:text-gray-200">Cart</a>
-</footer>
+<?php include 'footer.php'; ?>
+
 
 </body>
 </html>
---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer class="bg-[#247A73] text-white text-center p-4 mt-16">
+  &copy; 2025 SurfCity Collectibles. All rights reserved.
+  <br>
+  <a href="index.php" class="underline hover:text-gray-200">Home</a> |
+  <a href="shop.php" class="underline hover:text-gray-200">Shop</a> |
+  <a href="cart.php" class="underline hover:text-gray-200">Cart</a>
+</footer>
